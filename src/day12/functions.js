eb3a0ad90f14742d69c8841025087414a6504906
shaojuanzhang <rollingcomma@gmail.com>
2020-12-17 03:12:15 -0800

    finished day 12

--- src/day12/functions.js
+++ src/day12/functions.js
@@ -11,7 +11,7 @@ const readInput = (file) => {
 }
 
 /**
- * Description:
+ * Description: calculate the ship's orientation after rotating by given degree
  * 
  * ######################################
  *        N        
@@ -25,27 +25,27 @@ const readInput = (file) => {
  * 
  * Orientation array : [n, e, s, w]
  * Direction:  R - clockwise, index increase; 
- *             L - anticlockwise, index decrease
+ *             L - counter-clockwise, index decrease
  * if L turn results in negative index, add offset 4
  * 
  * 
  * @param {char} direction 
- * @param {int} angle 
+ * @param {int} degree 
  * @param {char} currentOrientation 
  * 
  * @returns {char} next orientation
  */
-const orientationCalculator = (direction, angle, currentOrientation) => {
+const orientationCalculator = (direction, degree, currentOrientation) => {
   const orientation = ["n","e", "s","w"];
   const currentIndex = orientation.indexOf(currentOrientation);
-  const indexOffSet = angle / 90;
+  const indexOffSet = degree / 90;
   const afterIndex = direction === "R" ? ((currentIndex + indexOffSet) % 4) : ((currentIndex - indexOffSet) % 4);
   return afterIndex >= 0? orientation[afterIndex]: orientation[4 + afterIndex];
 }
 
 const part1 = (inputs) => {
-  let position = {n:0, s:0, e:0, w:0};
-  let orentation = "e";
+  let position = {n:0, s:0, e:0, w:0},
+      orentation = "e";
   inputs.forEach(instruction => {
     switch (instruction[0]) {
       case "N": {
@@ -75,16 +75,101 @@ const part1 = (inputs) => {
       }
     }
   });
-  console.log(position)
   return Math.abs(position["s"] - position["n"]) + Math.abs(position["e"] - position["w"])
 }
 
+/**
+ * Description: calculate the waypoint postion when rotate R or L
+ *
+ * Rotating to L (counter-clockwise) n degree equals to rotating R (clockwise) 360 - n degree
+ * 
+ * Use counter-clockwise rotation matrix - @ degree 
+ * to calculate new position (x', y') after rotating by given degree
+ *
+ * x'   | cos@  -sin@ |   | x |
+ *    = |             | * |   |
+ * y'   | sin@   cos@ |   | y |
+ *
+ *  
+ * @param {char} direction 
+ * @param {int} degree 
+ * @param {object} currentPosition 
+ * 
+ * @returns {object} newPosition
+ */
+const waypointPositionCalculator = (direction, degree, currentPosition) => {
+  degree = degree % 360; //mod 
+  
+  //convert clockwise rotation to counter-clockwise rotation
+  if(direction === "R") {
+    degree = 360 - degree;
+  }
+  
+  //map orientation to (x, y) axis
+  const x = currentPosition["e"] - currentPosition["w"];
+  const y = currentPosition["n"] - currentPosition["s"];
+  
+  const newX = x * Math.round(Math.cos(degree * Math.PI / 180))
+                     - y * Math.round(Math.sin(degree * Math.PI / 180));
+  const newY = x * Math.round(Math.sin(degree * Math.PI / 180))
+                     + y * Math.round(Math.cos(degree * Math.PI / 180));
+
+  //map (x, y) position to orientation 
+  let newPosition = {n:0, s:0, e:0, w:0};
+  if(newX >= 0) { 
+    newPosition["e"] = newX;
+  } else {
+    newPosition["w"] = -newX;
+  }
+  if (newY >= 0) {
+    newPosition["n"] = newY;
+  } else {
+    newPosition["s"] = -newY;
+  }
+  return newPosition;
+}
 
 const part2 = (inputs) => {
-  
+  let shipPosition = { n: 0, s: 0, e: 0, w: 0 },
+      waypointPosition = { n:1, s:0, e:10, w:0 };
+      
+  inputs.forEach(instruction => {
+    switch (instruction[0]) {
+      case "N": {
+        waypointPosition["n"] += instruction[1]
+        break;
+      }
+      case "S": {
+        waypointPosition["s"] += instruction[1]
+        break;
+      }
+      case "E": {
+        waypointPosition["e"] += instruction[1]
+        break;
+      }
+      case "W": {
+        waypointPosition["w"] += instruction[1]
+        break;
+      }
+      case "F": {
+        for(const prop in shipPosition) {
+          shipPosition[prop] += instruction[1] * waypointPosition[prop];
+        }
+        break;
+      }
+      case "L": //fall through
+      case "R": {
+        waypointPosition = waypointPositionCalculator(instruction[0], instruction[1], waypointPosition);
+        break;
+      }
+    }
+  });
+  return Math.abs(shipPosition["s"] - shipPosition["n"]) + Math.abs(shipPosition["e"] - shipPosition["w"])
 }
 
 module.exports = {
   readInput, 
+  orientationCalculator,
+  waypointPositionCalculator,
   part1,
   part2 }
--- src/day12/functions.test.js
+++ src/day12/functions.test.js
@@ -1,6 +1,8 @@
 const { 
   readInput, 
+  orientationCalculator,
   part1,
+  waypointPositionCalculator,
   part2 } = require('./functions');
 
 describe('test day11 solution ', () => {
@@ -11,15 +13,31 @@ describe('test day11 solution ', () => {
     ["R",90],
     ["F",11]
   ];
-    test('readInput - read file line by line, convert each line to an array of instruction and argument', () => {
+
+  test('readInput - read file line by line, convert each line to an array of instruction and argument', () => {
     expect(readInput("testInput.txt")).toEqual(dataset);
   });
 
+  test('orientationCalculator - calculator orientation after turn a given angle', () => {
+    expect(orientationCalculator("R", 180, "e")).toEqual("w");
+    expect(orientationCalculator("R", 0, "e")).toEqual("e");
+    expect(orientationCalculator("R", 360, "e")).toEqual("e");
+    expect(orientationCalculator("R", 630, "e")).toEqual("n");
+    expect(orientationCalculator("L", 270, "e")).toEqual("s");
+    expect(orientationCalculator("L", 0, "e")).toEqual("e");
+    expect(orientationCalculator("L", 360, "e")).toEqual("e");
+    expect(orientationCalculator("L", 630, "e")).toEqual("s");
+  });
+  
   test("part1 - count the ship's Manhattan distance from its starting position", () => {
     expect(part1(dataset)).toBe(25);
   });
 
+  test('waypointPositionCalculator - calculator waypoint position after turn a given angle', () => {
+    expect(waypointPositionCalculator("R", 90, { n: 1, s: 0, e: 10, w: 0 })).toEqual({ n: 0, s: 10, e: 1, w: 0 });
+  });
+
   test('part2 - count number of occupied seats until no seats change state', () => {
-    //expect(part2(dataset)).toBe(26);
+    expect(part2(dataset)).toBe(286);
   });
 })
